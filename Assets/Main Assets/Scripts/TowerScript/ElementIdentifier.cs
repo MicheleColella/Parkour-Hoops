50f9727a21964cd0cc17b16323daabe2bc1f818f
Michele Colella <michelecolella0@gmail.com>
2024-11-15 18:57:06 +0100

    Aggiornato il sistema di generazione della torre

--- Assets/Main Assets/Scripts/TowerScript/ElementIdentifier.cs
+++ Assets/Main Assets/Scripts/TowerScript/ElementIdentifier.cs
@@ -3,4 +3,14 @@ using UnityEngine;
 public class ElementIdentifier : MonoBehaviour
 {
     public string elementID; // Assegna un ID univoco a ogni elemento nell'Inspector
+    public bool useGameObjectName = false; // Bool per usare il nome del GameObject, default false
+
+    private void Awake()
+    {
+        // Se il bool è attivo, usa il nome del GameObject come elementID
+        if (useGameObjectName)
+        {
+            elementID = gameObject.name;
+        }
+    }
 }
